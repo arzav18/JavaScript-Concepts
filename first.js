cbfa9a315ecc3fe163a521f7e8e5d1a9383aacb9
Arzav Jain <arzav.jain@gmail.com>
2024-05-28 16:18:18 +0530

    variables and data types

---
+++ first.js
@@ -0,0 +1,11 @@
+//Question: Create a const object to store information about the product
+
+const product = {
+    title: "Ball Pen",
+    price: 25,
+    isDiscount: true,
+    isOneDayDelivery: false,
+    color: "Black"
+};
+
+console.log(product);

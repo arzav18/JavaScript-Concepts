6b9e65593fff149e94a84020a57c7f58f7bc43ec
Arzav Jain <arzav.jain@gmail.com>
2024-06-25 16:15:48 +0530

    Events in JS

--- first.js
+++ first.js
@@ -1,104 +1,71 @@
-/* Functions: */
+/* Events in JS: The change in the state of an object is known as Event. Eg. click, submit etc. */
 
-//function to add 2 numbers:
-function addNum(num1, num2){
-    console.log(num1+num2);
+
+/* These are event handlers */
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () => {
+    console.log("btn1 was clicked");
+    let a = 25;
+    a++;
+    console.log(a);
 }
 
-addNum(1,2);
+let div = document.querySelector("#div");
 
-
-//Arrow function:
-
-const addNum = (num1, num2) => {
-    console.log(num1+num2);
-}
-addNum(1,2);
-
-
-//Count the number of vowels in a string
-function countVowel(str){
-    let count = 0;
-    for(const char of str){
-        if(char=='a' || char=='e' || char=='i' || char=='o' || char=='u'){
-            count++;
-        }
-    }
-    return count;
+div.onmouseover = () => {
+    console.log("mouse over box");
 }
 
 
-//Count vowel using arrow functions
+/* Event Objects: it is a special object that has details about the event. All event handlers have access
+to the event's properties and methods */
 
-const countVowel = (str) => {
-    let count = 0;
-    for(const char of str){
-        if(char=='a' || char=='e' || char=='i' || char=='o' || char=='u'){
-            count++;
-    }}
-    return count;
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = (e) => {
+    console.log(e);
+    console.log(e.type);
 }
 
 
-//ForEach loop in functions
+/* Event Listeners: used to write events more efficiently. Multiple operations can be performed on the same event. */
 
-let arr = [1,2,3,4,5];
-arr.forEach((val)=>{
-    console.log(val);
+/* Syntax: node.addEventListener(event, callback) */
+
+let btn1 = document.querySelector("#btn1");
+
+btn1.addEventListener("click", () => {
+    console.log("button was clicked");
+})
+
+btn1.addEventListener("click", () => {
+    console.log("button 1 was clicked");
 })
 
 
+/* Syntax: node.removeEventListener(event, callback) ; used to remove an event*/
 
-/* A callback is a function passed as an argument to another function: arr.forEach(callBackFunction) */
 
-
-// Print the square of each element in an array using forEach loop
-let arr = [1,2,3,4,5];
-
-arr.forEach((val) => {
-    console.log(val*val);
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - handler1");
 })
 
-
-/* Maps: creates a new array with the results of some operations: arr.map(callBackFunc(val, idx, arr)) */
-
-let arr = [1,2,3,4,5];
-
-let newArr = arr.map((val) => {
-    return val*2;
-})
-console.log(newArr);
-
-
-
-/* Filter: creates a new array of elements that give true for a condition/filter */
-
-//even numbers
-let arr = [1,2,3,4,5];
-
-let newArr = arr.filter((val) => {
-    return val%2==0;
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - handler2");
 })
 
-console.log(newArr);
+const handler3 = () => {
+    console.log("button1 was clicked - handler3")
+}
 
+btn1.addEventListener("click", handler3)
 
-
-/* Reduce: performs some operations, reduces the array to a single value. It returns that single value */
-
-//Return the largest element
-let arr = [1,2,3,4,5];
-
-let output = arr.reduce((prev, curr) => {
-    return prev>curr? prev:curr;
-})
-console.log(output);
-
-
-//Filter out the marks>=90
-let arr = [10,50,90,100,98];
-let newArr = arr.filter((val) => {
-    return val>=90;
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - handler4");
 })
 
-console.log(newArr);
+btn1.removeEventListener("click", handler3);
+
+
+/* Note: The callback reference should be same to remove */
